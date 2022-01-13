645926b9041a5405dffd681f940f3dcdabb6acb6
Rafael Castro <rafaelmp@ciandt.com>
2022-01-13 16:48:56 -0300

    Finalizando configuração inicial

--- src/main/java/br/com/wallet/transacao/cosumer/event/Transacao.java
+++ src/main/java/br/com/wallet/transacao/cosumer/event/Transacao.java
@@ -1,8 +1,14 @@
 package br.com.wallet.transacao.cosumer.event;
 
+import br.com.wallet.transacao.serializer.OffsetDateTimeDeserializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.OffsetDateTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,4 +18,8 @@ public class Transacao {
     private String idUsuario;
     private Long valorDebitado;
     private String contaOrigem;
+    private Long valorCreditado;
+    @JsonSerialize(using = OffsetDateTimeSerializer.class)
+    @JsonDeserialize(using = OffsetDateTimeDeserializer.class)
+    private OffsetDateTime timestamp;
 }
